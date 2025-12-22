70de6a75f2cf490f752c4fbeec3336d354eed3d3
Misat11 <20199703+Misat11@users.noreply.github.com>
2025-12-22 15:58:41 +0100

    build: update for latest builder changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://repo.screamingsandals.org/public/")
-        // TODO: remove repository when (if) uploaded to gradle plugin portal
-        maven("https://maven.neoforged.net/releases") {
-            content {
-                includeGroup("net.neoforged.licenser")
-            }
-        }
     }
 }
 
